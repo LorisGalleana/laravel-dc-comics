b8142dea4fd769000ccda601670a7249e18e5920
LorisGalleana <loris.galleana@gmail.com>
2024-09-17 20:46:33 +0200

    ESERCIZIO COMPLETATO

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -8,34 +8,35 @@
 @section('content')
 <div class="container my-5 bg-white">
     <h1>Nuovo fumetto</h1>
-    <form class="py-5" action="#" method="POST">
+    <form class="py-5" action="{{ route('comics.store') }}" method="POST">
+        @csrf
         <div class="mb-3">
             <label for="title" class="form-label">Titolo fumetto</label>
-            <input type="text" class="form-control" id="title" placeholder="Titolo" >
+            <input type="text" class="form-control" id="title" name="title" placeholder="Titolo" >
         </div>
         <div class="mb-3">
             <label for="src" class="form-label">Immagine fumetto</label>
-            <input type="text" class="form-control" id="src" placeholder="Percorso immagine" >
+            <input type="text" class="form-control" id="src" name="thumb" placeholder="Percorso immagine" >
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Descrizione fumetto</label>
-            <textarea class="form-control" id="description" cols="30" rows="5" placeholder="Descrizione" ></textarea>
+            <textarea class="form-control" id="description" cols="30" rows="5" name="description" placeholder="Descrizione" ></textarea>
         </div>
         <div class="mb-3">
             <label for="price" class="form-label">Titolo fumetto</label>
-            <input type="text" class="form-control" id="price" placeholder="Prezzo" >
+            <input type="text" class="form-control" id="price" name="price" placeholder="Prezzo" >
         </div>
         <div class="mb-3">
             <label for="series" class="form-label">Serie fumetto</label>
-            <input type="text" class="form-control" id="series" placeholder="Serie" >
+            <input type="text" class="form-control" id="series" name="series" placeholder="Serie" >
         </div>
         <div class="mb-3">
             <label for="date" class="form-label">Data uscita fumetto</label>
-            <input type="date" class="form-control" id="title" placeholder="data di uscita" >
+            <input type="date" class="form-control" id="date" name="sale_date" placeholder="data di uscita" >
         </div>
         <div class="mb-3">
             <label for="type" class="form-label">Tipo fumetto</label>
-            <input type="text" class="form-control" id="type" placeholder="Tipo" >
+            <input type="text" class="form-control" id="type" name="type" placeholder="Tipo" >
         </div>
         <div class="mb-3">
             <button type="submit" href="#" class="btn btn-success">Invia</button>
